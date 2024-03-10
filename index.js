6271817a8ad974627cd316a4faf1b8b92cb1e511
kevin aviles <kevinaviles140@gmail.com>
2024-03-09 20:03:16 -0500

    commit array numerica usando metodo

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ let arrayConElementos = [1, 2, 3, 4, 5];
 let arrayDiferentesTipos = [1, 'dos', true, { nombre: 'Juan' }];
 //Crear un array utilizando el constructor Array
 let arrayConstructor = new Array(3);
+//Crear un array con una secuencia numérica usando el método
+let secuenciaNumerica = Array.from({ length: 5 }, (_, index) => index + 1);
