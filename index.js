3f52e88699e69ce4ed5fdf26c73d0717b067c7a1
kevin aviles <kevinaviles140@gmail.com>
2024-03-09 19:54:24 -0500

    commit array elementos tipo datos

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 let arrayVacio = [];
 //Crear un array con elementos
 let arrayConElementos = [1, 2, 3, 4, 5];
+//Crear un array con elementos de diferentes tipos de datos
+let arrayDiferentesTipos = [1, 'dos', true, { nombre: 'Juan' }];
